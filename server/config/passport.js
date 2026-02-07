53df01ee39cd3477f62a51b3b21e3fa0784301b8
Zion203 <jesudaszion203@gmail.com>
2026-02-07 10:42:20 +0530

    bug fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ const configurePassport = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:5000/auth/google/callback',
+        callbackURL: process.env.API_URL + '/auth/google/callback',
         proxy: true,
       },
       async (accessToken, refreshToken, profile, done) => {
